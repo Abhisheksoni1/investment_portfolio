b387e5a8b37d3d4df075dcbc5e45101f0436b351
abhisheksoni <abhisheksoni500@gmail.com>
2018-01-04 13:51:57 +0530

    heroku customization done

--- investment_portfolio/settings.py
+++ investment_portfolio/settings.py
@@ -101,11 +101,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
