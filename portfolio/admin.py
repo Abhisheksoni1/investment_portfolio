a5da61cd3ab7eeffe2554b1e73f41ed78d79a2f1
abhisheksoni <abhisheksoni500@gmail.com>
2018-01-13 14:40:07 +0530

    changeform_view customize

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -23,8 +23,24 @@ class FundAdmin(admin.ModelAdmin):
             qs = response.context_data['cl']
         except (AttributeError, KeyError):
             return response
-
         return response
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        response = super(FundAdmin, self).changeform_view(request, object_id=object_id,
+                         form_url=form_url, extra_context=extra_context)
+        try:
+            fund_obj = Fund.objects.get(id=object_id)
+            funds = Fund.objects.filter(portfolio=fund_obj.portfolio, user=fund_obj.user, fund_type=fund_obj.fund_type,
+                                        date__lte=fund_obj.date.date())
+            funds = funds[::-1]
+            response.context_data.update({'data': funds[0]})
+        except Exception as e:
+            print(e)
+            return response
+        return response
+
+
+
 
 
 admin.site.register(Fund, FundAdmin)
