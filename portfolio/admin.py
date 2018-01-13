010c8096c0e031e5c29f1d0b614753c63221564d
abhisheksoni <abhisheksoni500@gmail.com>
2018-01-13 21:56:45 +0530

    admin configure

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -9,6 +9,17 @@ admin.site.register(Profile)
 admin.site.register(Portfolio)
 
 
+def parse_obj(obj):
+    obj_dict = {'total_injected': float(obj.total_injected),
+                'total_asset': float(obj.total_asset),
+                'shares': float(obj.shares),
+                'cumul_realized': float(obj.cumul_realized),
+                'nav_share': float(obj.nav_share),
+                'cumul_variation': float(obj.cumul_variation)
+             }
+    return obj_dict
+
+
 class FundAdmin(admin.ModelAdmin):
     list_display = ('fund_type', 'portfolio', 'user', 'cash_balance', 'cost', 'market_value', 'total_asset',
                     'realized', 'gross_nav', 'expenses', 'net_nav', 'shares',)
@@ -35,20 +46,45 @@ class FundAdmin(admin.ModelAdmin):
                 funds = funds[::-1]
                 response.context_data.update({'data': funds[0]})
             else:
-                crypto_funds = Fund.objects.filter(fund_type='crypto')[::-1]
-                stock_funds = Fund.objects.filter(fund_type='stocks')[::-1]
-                crypto, crypto1, stock = None, None, None
-                if crypto_funds:
-                    crypto = json.dumps({'key1': 'hello', 'key2': 2.0, 'key3': 3})
-                    crypto1 = json.dumps({'fund_type': crypto_funds[0].fund_type,
-                                         'portfolio': crypto_funds[0].portfolio.name,
-                                         'buying': float(crypto_funds[0].buying)})
-                if stock_funds:
-                    stock = json.dumps(model_to_dict(stock_funds[0]))
-                response.context_data.update({'stock': stock or '',
-                                              'crypto': crypto or '',
-                                              'test': crypto1})
-                print(response.context_data['crypto'])
+                funds = Fund.objects.all()
+                portoliio = Portfolio.objects.all()
+                crypto, stock = {}, {}
+                for i in portoliio:
+                    crypto_fund = funds.filter(portfolio=i, fund_type='crypto', user=request.user)
+                    stock_fund = funds.filter(portfolio=i, fund_type='stocks', user=request.user)
+                    if crypto_fund:
+                        crypto_fund = crypto_fund[::-1]
+                        crypto.update({i.name: parse_obj(crypto_fund[0])})
+                    else:
+                        crypto.update({i.name: ''})
+                    if stock_fund:
+                        stock_fund = stock_fund[::-1]
+                        stock.update({i.name: parse_obj(stock_fund[0]).update({'set': float(stock_fund[0].set)})})
+                    else:
+                        stock.update({i.name: ''})
+                print(json.dumps(crypto))
+                print(json.dumps(stock))
+
+                response.context_data.update({'crypto': crypto,
+                                              'stocks': stock})
+
+                # crypto_funds = ''
+                # stock_funds = ''
+                # crypto, crypto1, stock = None, None, None
+                # print(crypto_funds)
+                # print(stock_funds)
+                # print(btc)
+                # if crypto_funds:
+                #     crypto = json.dumps({'key1': 'hello', 'key2': 2.0, 'key3': 3})
+                #     crypto1 = json.dumps({'fund_type': crypto_funds[0].fund_type,
+                #                          'portfolio': crypto_funds[0].portfolio.name,
+                #                          'buying': float(crypto_funds[0].buying)})
+                # if stock_funds:
+                #     stock = json.dumps(model_to_dict(stock_funds[0]))
+                # response.context_data.update({'stock': stock or '',
+                #                               'crypto': crypto or '',
+                #                               'test': crypto1})
+                # print(response.context_data['crypto'])
         except Exception as e:
             print(e)
             return response
